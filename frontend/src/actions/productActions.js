b3b2d6a60f6e3b907806169a62d0922dac8c645e
nvkex <scrbwrm@gmail.com>
2020-09-05 19:09:47 +0530

    Actions

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } = require("../constants/productConstants")
+
+const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUEST });
+        const { data } = await axios.get('/api/products');
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    }
+    catch (e) {
+        dispatch({ type: PRODUCT_LIST_FAIL, payload: e.message });
+    }
+}
+
+export { listProducts };

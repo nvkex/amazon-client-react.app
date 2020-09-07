08b90c960281588c1154ee8c9eedee917d5223d3
nvkex <scrbwrm@gmail.com>
2020-09-07 16:53:30 +0530

    product details route

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,20 @@ const data = require('./data');
 
 const app = express();
 
+
+app.get('/api/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if(product)
+        res.send(product);
+    else
+        res.status(404).send({msg: 'Product not found.'});
+});
+
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
+
 
 app.listen(5000, () => {
     console.log('Running on 5000')

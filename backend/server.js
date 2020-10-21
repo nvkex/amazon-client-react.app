3cf7956968ab07a54ff7818513418a3081d06ca7
nvkex <scrbwrm@gmail.com>
2020-10-21 17:30:34 +0530

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const data = require('./data');
+const config = require('./config');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const userRouter = require('./routes/user.route');
 
 const app = express();
 
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
 
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch( err => console.log(err.reason))
+
+
+app.use('/api/users', userRouter)
 app.get('/api/products/:id', (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x => x._id === productId);
